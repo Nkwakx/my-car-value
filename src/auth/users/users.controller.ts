9b6d55881f066aed65a1d1723c0f6e3047210898
Nhlakanipho Khumalo <nhlakanipho@developmenthub.co.za>
2022-08-11 15:12:13 +0200

    auth-commit

--- src/auth/users/users.controller.ts
+++ src/auth/users/users.controller.ts
@@ -1,10 +1,13 @@
+import { AuthGuard } from './../../guards/auth.guard';
 import { CreateUserDto } from './dtos/create-user.dto';
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './../users/dtos/user.dto';
 import { Serialize } from '../../interceptors/serialize.interceptor';
 import { AuthService } from '../auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entitiy';
 
 
 @Controller('auth')
@@ -16,10 +19,17 @@ export class UsersController {
           private authService: AuthService
      ) { }
 
+     // @Get('/whoami')
+     // whoAmI(@Session() session: any){
+     //      return this.usersService.findOne(session.userId);
+     // }
+
      @Get('/whoami')
-     whoAmI(@Session() session: any){
-          return this.usersService.findOne(session.userId);
+     @UseGuards(AuthGuard)
+     whoAmI(@CurrentUser() user: User){
+          return user;
      }
+
      @Post('/signout')
      signOut(@Session() session: any){
           session.userId = null
--- src/auth/users/users.module.ts
+++ src/auth/users/users.module.ts
@@ -1,9 +1,11 @@
+import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { User } from './user.entitiy';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from '../auth.service';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 
 
 @Module({
@@ -11,6 +13,13 @@ import { AuthService } from '../auth.service';
     TypeOrmModule.forFeature([User])
   ],
   controllers: [UsersController],
-  providers: [UsersService, AuthService]
+  providers: [
+    UsersService,
+    AuthService, 
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor
+    },
+  ]
 })
-export class UsersModule {}
+export class UsersModule { }
