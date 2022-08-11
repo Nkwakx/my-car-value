fac5dfa269832e2ddbe223c22c70d3eb72396b32
Nhlakanipho Khumalo <nhlakanipho@developmenthub.co.za>
2022-08-11 17:02:27 +0200

    report-commit

--- src/auth/users/user.entitiy.ts
+++ src/auth/users/user.entitiy.ts
@@ -1,9 +1,10 @@
-import { Entity, Column,  PrimaryGeneratedColumn, AfterInsert, AfterRemove, AfterUpdate } from 'typeorm';
+import { Report } from './../../reports/report.entity';
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterRemove, AfterUpdate, OneToMany } from 'typeorm';
 // import { Exclude } from 'class-transformer';
 
 @Entity()
 export class User {
-     
+
      @PrimaryGeneratedColumn()
      id: number;
 
@@ -14,18 +15,21 @@ export class User {
      @Column()
      password: string;
 
+     @OneToMany(() => Report, (report) => report.user)
+     reports: Report[]
+
      @AfterInsert()
-     logInsert(){
+     logInsert() {
           console.log('Inserted User with id', this.id)
      }
 
      @AfterUpdate()
-     logUpdate(){
+     logUpdate() {
           console.log('Updated user with id', this.id)
      }
 
      @AfterRemove()
-     logRemove(){
+     logRemove() {
           console.log('Removed user with id', this.id)
      }
 }
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { ReportDto } from './dtos/report.dto';
+import { User } from './../auth/users/user.entitiy';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../auth/users/decorators/current-user.decorator';
+import { Serialize } from '../interceptors/serialize.interceptor';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+constructor(private reportsService: ReportsService){}
+
+     @Post()
+     @UseGuards(AuthGuard)
+     @Serialize(ReportDto)
+     createReport(@Body() body: CreateReportDto, @CurrentUser() user: User){
+          return this.reportsService.create(body, user);
+     }
+}
+
